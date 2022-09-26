8d231c3d1be1a1a8faa9943375707d6d8a8e9f5e
Kirill Velichko <cirill@live.ru>
2022-09-26 13:50:46 +0300

    Prewritten registration service (without unit tests so far)

--- PolusGold/AuthorServTemplate/Polus/auth.cpp
+++ PolusGold/AuthorServTemplate/Polus/auth.cpp
@@ -1,5 +1,11 @@
 #include "auth.hpp"
 #include <openssl/rand.h>
+#include <cwctype>
+#include <string>
+#include <optional>
+#include <locale>
+#include <ctype.h>
+#include <uchar.h>
 using namespace std::string_literals;
 namespace Polus{
 AuthTempl::AuthTempl(const TableBasicData& data) : dataBaseTempl(data){
@@ -8,60 +14,129 @@ AuthTempl::AuthTempl(const TableBasicData& data) : dataBaseTempl(data){
 
 }
 bool AuthTempl::IsInclude(const RegData& data,  mysqlx::Table& table) const{
-    auto row = table.select(L"email"s).where(L"email like :email"s).bind(L"email"s, std::get<0>(data)).execute();
+    auto row = table.select("email").where("email like :email").bind("email", std::get<0>(data).c_str()).execute();
     return row.count() != 0;
 }
-//стоит дописать проверку вводимых пользователем данных
-RegistState AuthTempl::TryToRegistr(const RegData& data) const{
-    RegistState result = RegistState::Ok;
-    try{
-        auto schema = this->session->getSchema(this->dataBaseTempl.dbName);
-        auto table = schema.getTable(this->dataBaseTempl.tableName);
-        if(this->IsInclude(data, table)){
-            result = RegistState::AlreadyRegistered;
+bool CheckUsername(std::u16string_view view){
+    if(view.size() < 5 || view.size() > 19){
+        return false;
+    }
+    auto& facet = std::use_facet<std::ctype<char16_t>>(std::locale());
+    auto IsSymbOkPred = [&facet](char16_t symb){
+        symb = facet.tolower(symb);
+        return bool(symb >= u'a' && symb <= u'z')|| bool(symb >= u'а' && symb <= u'я') || bool(symb == u'ё') ||
+                bool(symb >= u'0' && symb <= u'9') || bool(symb == u'_');
+
+    };
+    bool result = true;
+
+    for(auto cur = view.cbegin(); cur != view.cend(); cur = std::next(cur, 1)){
+        if(!IsSymbOkPred(*cur)){
+            result = false;
+            break;
         }
-        //refactor this shit!!!
-        //во-первых, надо захешировать данные с солью.
-        //во-вторых, надо преобразовать соль в utf-формат и сохранить в бд
-        else{
-            const std::size_t bCount = sizeof(wchar_t) * 15;
-            unsigned char buf[bCount];
-            auto written = RAND_bytes(buf, bCount);
-            if(written == 1){
-                std::wstring salt;
-                for(auto cur = buf; cur < buf + bCount; cur = cur + sizeof(wchar_t)){
-                    wchar_t saltSymb;
-                    std::memcpy(&saltSymb, cur, sizeof(wchar_t));
-                    salt.push_back(saltSymb);
+    }
+    return result;
+}
+bool CheckPassword(std::u16string_view view){
+    if(view.size() < 8 || view.size() > 30){
+        return false;
+    }
+    bool result = true;
+    auto& facet = std::use_facet<std::ctype<char16_t>>(std::locale());
+
+    for(auto cur = view.cbegin(); cur != view.cend(); cur = std::next(cur,1)){
+        if(facet.is(facet.space, *cur)){
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+bool CheckEmail(std::u16string_view view){
+    bool result = true;
+    if(view.length() > 129 || view.length() < 3){
+        result = false;
+    }
+    else{
+        auto IsOkSymb = [isDogHere = false](auto symb) mutable{
+            if(symb == u'@'){
+                if(!isDogHere){
+                    isDogHere = true;
+                    return true;
                 }
-                std::size_t NonSoltedSz = std::get<2>(data).length() * sizeof(wchar_t);
-                std::size_t salted1Size = NonSoltedSz + bCount;
-                unsigned char saltedBuf1[salted1Size];
-                std::memcpy(saltedBuf1, std::get<2>(data).c_str(), NonSoltedSz);
-                std::memcpy(saltedBuf1 + NonSoltedSz, buf, bCount);
-                unsigned char hash1[SHA256_DIGEST_LENGTH];
-                unsigned char hash2[SHA256_DIGEST_LENGTH];
-                SHA256_CTX ctx1;
-                SHA256_Init(&ctx1);
-                SHA256_Update(&ctx1, saltedBuf1, salted1Size);
-                SHA256_Final(hash1, &ctx1);
-                SHA256_CTX ctx2;
-                //прототип глобальной соли, потом сделать через макрос с конфигов!!!
-                auto globalSalt = L"qwert"s;
-                std::size_t saltGlSize = globalSalt.length() * sizeof(wchar_t);
-                std::size_t salted2Size = SHA256_DIGEST_LENGTH + saltGlSize;
-                unsigned char salted2Buf[salted2Size];
-                std::memcpy(salted2Buf, hash1, SHA256_DIGEST_LENGTH);
-                std::memcpy(salted2Buf + SHA256_DIGEST_LENGTH, globalSalt.c_str(), saltGlSize);
-                SHA256_Init(&ctx2);
-                SHA256_Update(&ctx2, salted2Buf, salted2Size);
-                SHA256_Final(hash2, &ctx2);
-
+                else{
+                    return false;
+                }
             }
+            return (symb >= u'a' && symb <= u'z')  || symb == u'-' || symb == u'+' || symb == u'\'' ||
+                    (symb >= u'0' && symb <= u'9') || symb == u'_' || symb == u'.';
 
+        };
+        for(auto cur = view.cbegin(); cur != view.cend(); cur = std::next(cur, 1)){
+            if(!IsOkSymb(*cur)){
+                result = false;
+                break;
+            }
         }
-    } catch(mysqlx::Error& e){
+    }
+
+    return result;
+}
+bool CheckRegData(const RegData& data){
+    return CheckEmail(std::get<0>(data)) && CheckUsername(std::get<1>(data)) && CheckPassword(std::get<2>(data));
+}
+//стоит дописать проверку вводимых пользователем данных
+RegistState AuthTempl::TryToRegistr(const RegData& data, std::string_view globalSalt) const{
+    RegistState result = RegistState::Ok;
+    if(!CheckRegData(data)){
         result = RegistState::UncorrectLoginOrPassword;
+    }
+    else{
+        try{
+            auto schema = this->session->getSchema(this->dataBaseTempl.dbName);
+            auto table = schema.getTable(this->dataBaseTempl.tableName);
+            if(this->IsInclude(data, table)){
+                result = RegistState::AlreadyRegistered;
+            }
+            else{
+                const std::size_t bCount = 30;
+                unsigned char salt[bCount];
+                auto written = RAND_bytes(salt, bCount);
+                if(written == 1){
+                    std::size_t NonSoltedSz = std::get<2>(data).length() * sizeof(char16_t);
+                    const std::size_t salted1Size = NonSoltedSz + bCount;
+                    std::unique_ptr<unsigned char[]> saltedBuf1 = std::make_unique<unsigned char[]>(salted1Size);
+                    std::memcpy(saltedBuf1.get(), std::get<2>(data).c_str(), NonSoltedSz);
+                    std::memcpy(saltedBuf1.get() + NonSoltedSz, salt, bCount);
+                    unsigned char hash1[SHA256_DIGEST_LENGTH];
+                    unsigned char hash2[SHA256_DIGEST_LENGTH];
+                    SHA256_CTX ctx1;
+                    SHA256_Init(&ctx1);
+                    SHA256_Update(&ctx1, saltedBuf1.get(), salted1Size);
+                    SHA256_Final(hash1, &ctx1);
+                    SHA256_CTX ctx2;
+                    std::size_t saltGlSize = globalSalt.length();
+                    const size_t salted2Size = SHA256_DIGEST_LENGTH + saltGlSize;
+                    std::unique_ptr<unsigned char[]> saltedBuf2 = std::make_unique<unsigned char[]>(salted2Size);
+                    std::memcpy(saltedBuf2.get(), hash1, SHA256_DIGEST_LENGTH);
+                    std::memcpy(saltedBuf2.get() + SHA256_DIGEST_LENGTH, globalSalt.data(), saltGlSize);
+                    SHA256_Init(&ctx2);
+                    SHA256_Update(&ctx2, saltedBuf2.get(), salted2Size);
+                    SHA256_Final(hash2, &ctx2);
+                    table.insert("id", "email", "username", "passH", "salt").values("NULL",
+                                 L"a", L"d", (char*)(hash2), (char*)salt);
+                }
+                else{
+                    throw std::runtime_error("OpenSSL gen error");
+                }
+            }
+        } catch(mysqlx::Error& e){
+            result = RegistState::UncorrectLoginOrPassword;
+        }
+        catch(std::runtime_error& er){
+            throw er;
+        }
     }
     return result;
 }
--- PolusGold/AuthorServTemplate/Polus/auth.hpp
+++ PolusGold/AuthorServTemplate/Polus/auth.hpp
@@ -12,11 +12,11 @@ enum class RegistState : std::size_t{
     Ok, AlreadyRegistered, UncorrectLoginOrPassword
 };
 struct TableBasicData{
-    std::wstring dbName;
-    std::wstring tableName;
+    std::u16string dbName;
+    std::u16string tableName;
 };
 //почта, username, password
-using RegData = std::tuple<std::wstring, std::wstring, std::wstring>;
+using RegData = std::tuple<std::u16string, std::u16string, std::u16string>;
 class AuthTempl {
 private:
     TableBasicData dataBaseTempl;
@@ -24,6 +24,6 @@ private:
     bool IsInclude(const RegData& data, mysqlx::Table& table) const;
 public:
     AuthTempl(const TableBasicData& db);
-    RegistState TryToRegistr(const RegData& data) const;
+    RegistState TryToRegistr(const RegData& data, std::string_view globalSalt) const;
 };
 }
